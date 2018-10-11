9671ee3c40cf9a9a83d37ac56ea2e0cfa8826932
Sebastiaan van Wagensveld <sebastiaan@SebastiaannoMacBook-puro.local>
2018-10-11 18:53:12 +1100

    Cleaned up hover in js

--- js/tentacle.js
+++ js/tentacle.js
@@ -1,12 +1,37 @@
-    function changeText(text)
-        {
-            var display = document.getElementById('myTitle')
-            display.innerHTML = ""
-            display.innerHTML = text
-        }
-          function changeback(text)
-        {
-            var display = document.getElementById('myTitle')
-            display.innerHTML = ""
-            display.innerHTML = text
-        }
+// Changing title functionality
+
+function changeText (text) {
+  document.getElementById('myTitle').innerHTML = text
+}
+
+document.getElementById('github').onmouseover = function () {
+  changeText('<h1>40Torps</h1>')
+}
+
+document.getElementById('github').onmouseout = function () {
+  changeText('<h1>40Torps</h1>')
+}
+
+document.getElementById('discord').onmouseover = function () {
+  changeText('<h1>Sebastiaan#1863</h1>')
+}
+
+document.getElementById('discord').onmouseout = function () {
+  changeText('<h1>40Torps</h1>')
+}
+
+document.getElementById('tumblr').onmouseover = function () {
+  changeText('<h1>ore-imouto</h1>')
+}
+
+document.getElementById('tumblr').onmouseout = function () {
+  changeText('<h1>40Torps</h1>')
+}
+
+document.getElementById('twitter').onmouseover = function () {
+  changeText('<h1>@40Torps</h1>')
+}
+
+document.getElementById('twitter').onmouseout = function () {
+  changeText('<h1>40Torps</h1>')
+}
