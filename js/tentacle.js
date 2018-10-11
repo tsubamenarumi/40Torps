d1fd0ba9b72f91e27cf41622a30943a069f44784
Sebastiaan van Wagensveld <sebastiaan@SebastiaannoMacBook-puro.local>
2018-10-11 21:20:55 +1100

    Adding js to block

--- js/tentacle.js
+++ js/tentacle.js
@@ -1,37 +1,42 @@
+// Detecting mobile
+var tap = ('ontouchstart' in document.documentElement)
+
 // Changing title functionality
 
 function changeText (text) {
   document.getElementById('myTitle').innerHTML = text
 }
 
-document.getElementById('github').onmouseover = function () {
-  changeText('<h1>40Torps</h1>')
-}
+if (!tap) {
+  document.getElementById('github').onmouseover = function () {
+    changeText('<h1>40Torps</h1>')
+  }
 
-document.getElementById('github').onmouseout = function () {
-  changeText('<h1>40Torps</h1>')
-}
+  document.getElementById('github').onmouseout = function () {
+    changeText('<h1>40Torps</h1>')
+  }
 
-document.getElementById('discord').onmouseover = function () {
-  changeText('<h1>Sebastiaan#1863</h1><br><h2>Come join my server: 2 Moe 4 Festo</h2>')
-}
+  document.getElementById('discord').onmouseover = function () {
+    changeText('<h1>Sebastiaan#1863</h1><br><h2>Come join my server: 2 Moe 4 Festo</h2>')
+  }
 
-document.getElementById('discord').onmouseout = function () {
-  changeText('<h1>40Torps</h1>')
-}
+  document.getElementById('discord').onmouseout = function () {
+    changeText('<h1>40Torps</h1>')
+  }
 
-document.getElementById('tumblr').onmouseenter = function () {
-  changeText('<h1>ore-imouto</h1>')
-}
+  document.getElementById('tumblr').onmouseenter = function () {
+    changeText('<h1>ore-imouto</h1>')
+  }
 
-document.getElementById('tumblr').onmouseleave = function () {
-  changeText('<h1>40Torps</h1>')
-}
+  document.getElementById('tumblr').onmouseout = function () {
+    changeText('<h1>40Torps</h1>')
+  }
 
-document.getElementById('twitter').onmouseover = function () {
-  changeText('<h1>@40Torps</h1>')
-}
+  document.getElementById('twitter').onmouseover = function () {
+    changeText('<h1>@40Torps</h1>')
+  }
 
-document.getElementById('twitter').onmouseout = function () {
-  changeText('<h1>40Torps</h1>')
+  document.getElementById('twitter').onmouseout = function () {
+    changeText('<h1>40Torps</h1>')
+  }
 }
