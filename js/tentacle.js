36a0f7ac58415c09f9b44928cdf717f9fd81571f
Sebastiaan van Wagensveld <sebastiaan@SebastiaannoMacBook-puro.local>
2018-10-11 21:54:03 +1100

    Adding sweet hovering effect

--- js/tentacle.js
+++ js/tentacle.js
@@ -1,27 +1,30 @@
-// Detecting mobile
+// General vars
+var Title = '<h1>40Torps</h1>'
+
+// Detecting touch devices
 var tap = ('ontouchstart' in document.documentElement)
 
 // Changing title functionality
-
 function changeText (text) {
   document.getElementById('myTitle').innerHTML = text
 }
 
+// Changing titles so long as it's not a touch device
 if (!tap) {
-  document.getElementById('github').onmouseover = function () {
-    changeText('<h1>40Torps</h1>')
-  }
-
-  document.getElementById('github').onmouseout = function () {
-    changeText('<h1>40Torps</h1>')
-  }
-
   document.getElementById('discord').onmouseover = function () {
     changeText('<h1>Sebastiaan#1863</h1><br><h2>Come join my server: 2 Moe 4 Festo</h2>')
   }
 
   document.getElementById('discord').onmouseout = function () {
+    changeText(Title)
+  }
+
+  document.getElementById('github').onmouseover = function () {
     changeText('<h1>40Torps</h1>')
+  }
+
+  document.getElementById('github').onmouseout = function () {
+    changeText(Title)
   }
 
   document.getElementById('tumblr').onmouseenter = function () {
@@ -29,7 +32,7 @@ if (!tap) {
   }
 
   document.getElementById('tumblr').onmouseout = function () {
-    changeText('<h1>40Torps</h1>')
+    changeText(Title)
   }
 
   document.getElementById('twitter').onmouseover = function () {
@@ -37,6 +40,6 @@ if (!tap) {
   }
 
   document.getElementById('twitter').onmouseout = function () {
-    changeText('<h1>40Torps</h1>')
+    changeText(Title)
   }
 }
