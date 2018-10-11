8641e17340380a46aaf58c49ce052d2b2af849d5
Sebastiaan van Wagensveld <sebastiaan@SebastiaannoMacBook-puro.local>
2018-10-11 12:04:03 +1100

    First demo of the website

---
+++ js/tentacle.js
@@ -0,0 +1,12 @@
+    function changeText(text)
+        {
+            var display = document.getElementById('myTitle')
+            display.innerHTML = ""
+            display.innerHTML = text
+        }
+          function changeback(text)
+        {
+            var display = document.getElementById('myTitle')
+            display.innerHTML = ""
+            display.innerHTML = text
+        }
